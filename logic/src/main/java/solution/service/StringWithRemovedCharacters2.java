d182ff69a086b614d52fa194e2398d5c9facc179
Oleg Demura <demonshine1983@gmail.com>
2020-03-23 08:07:22 +0300

    Fixed sberbanktask2

--- logic/src/main/java/solution/service/StringWithRemovedCharacters2.java
+++ logic/src/main/java/solution/service/StringWithRemovedCharacters2.java
@@ -7,13 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class StringWithRemovedCharacters2 {
 
-  /*public Character returnCharacter(Map<Character, Integer> map) {
-    return map.entrySet().stream()
-        .max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1)
-        .get()
-        .getKey();
-  }*/
-
   public Integer returnMax(Map<Character, Integer> map) {
     return map.values().stream().max(Comparator.naturalOrder()).get();
   }
